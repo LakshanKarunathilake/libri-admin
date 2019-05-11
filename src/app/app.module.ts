16fcc36311175bd188767f08dccae11a7619d9a9
Lakshan Karunathilake <manualsplash@gmail.com>
2019-05-11 22:12:06 +0530

    Add firebase config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,19 @@ import {LoginComponent} from './pages/login/login.component';
 import {MatInputModule, MatCardModule, MatButton, MatButtonModule} from '@angular/material';
 import {AgmCoreModule} from '@agm/core';
 import {AdminLayoutComponent} from './layouts/admin-layout/admin-layout.component';
+import {AngularFireModule} from '@angular/fire';
+import {AngularFireFunctionsModule} from '@angular/fire/functions';
+import {AngularFirestoreModule} from '@angular/fire/firestore';
+
+const firebase = {
+  apiKey: 'AIzaSyDuPVBd60jVbPw1BRefZsTkWb2AXZsfzNk',
+  authDomain: 'libri-238805.firebaseapp.com',
+  databaseURL: 'https://libri-238805.firebaseio.com',
+  projectId: 'libri-238805',
+  storageBucket: 'libri-238805.appspot.com',
+  messagingSenderId: '696789073783',
+  appId: '1:696789073783:web:3579e974bc60674c'
+};
 
 @NgModule({
   imports: [
@@ -25,7 +38,10 @@ import {AdminLayoutComponent} from './layouts/admin-layout/admin-layout.componen
     ReactiveFormsModule,
     MatInputModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    AngularFireModule.initializeApp(firebase),
+    AngularFireFunctionsModule,
+    AngularFirestoreModule
   ],
   declarations: [AppComponent, AdminLayoutComponent, LoginComponent],
   providers: [],
