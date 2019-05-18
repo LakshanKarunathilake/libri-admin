6562dc01117c86d12b498829c0f094d5f4d731be
Lakshan Karunathilake <manualsplash@gmail.com>
2019-05-18 22:29:57 +0530

    Create table to view the registered users

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,4 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {MatPaginator, MatTableDataSource, MatSort} from '@angular/material';
+import {AngularFireFunctions} from '@angular/fire/functions';
+export interface UserData {
+  email: string;
+  name: string;
+  verified: boolean;
+  disabled: boolean;
+  lastLogin: Date;
+  created: Date;
+}
 
 @Component({
   selector: 'app-users',
@@ -6,10 +16,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.component.scss']
 })
 export class UsersComponent implements OnInit {
-
-  constructor() { }
+  displayedColumns: string[] = ['email', 'name', 'verified', 'disabled', 'lastLogin', 'created'];
+  dataSource: MatTableDataSource<UserData>;
+  isLoadingResults = true;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+  constructor(private aff: AngularFireFunctions) {}
 
   ngOnInit() {
+    // Calling the cloud functions to retrieve the user details of the registered users
+    this.aff.functions
+      .httpsCallable('getRegisteredUsers')()
+      .then(({data}) => {
+        console.log(data);
+        // Adding the data to the data source for the table
+        const users = Object.values(data);
+        // Deactivating spinner
+        this.isLoadingResults = false;
+        console.log('users', users);
+        this.dataSource = new MatTableDataSource(users);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
+      });
   }
 
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
 }
