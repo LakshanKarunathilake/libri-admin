4647c05c9b60df6b2ed62892daeabbcaa3918c26
Lakshan Karunathilake <manualsplash@gmail.com>
2019-10-14 21:24:43 +0530

    Add table view for book transfer requests

--- src/app/components/transfers/transfers.component.ts
+++ src/app/components/transfers/transfers.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {OverallInfoService} from 'app/services/overallInfo/overall-info.service';
+import {MatPaginator, MatSort, MatTableDataSource} from '@angular/material';
+import {Transfer} from 'app/models/Transfer';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-transfers',
@@ -6,10 +10,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./transfers.component.scss']
 })
 export class TransfersComponent implements OnInit {
-
-  constructor() { }
+  displayedColumns: string[] = ['issue_id', 'date_due', 'renewals', 'title', 'issuedate', 'status'];
+  dataSource: MatTableDataSource<Transfer>;
+  isLoadingResults = true;
+  transferRequests: Observable<Transfer[]>;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+  constructor(private info: OverallInfoService) {}
 
   ngOnInit() {
+    this.transferRequests = this.info.getTransferRequests();
+    this.transferRequests.subscribe(data => {
+      const requests = Object.keys(data).map(key => data[key]);
+      console.log('requests', requests);
+      this.dataSource = new MatTableDataSource(requests);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+      this.isLoadingResults = false;
+    });
   }
 
+  /**
+   * Filter the data source
+   * @param filterValue
+   */
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
 }
