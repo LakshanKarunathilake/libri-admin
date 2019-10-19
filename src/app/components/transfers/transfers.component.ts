1f47d4cd638de746e47068a38fbb80be6f3b8741
lakshan <manualsplash@gmail.com>
2019-10-20 01:08:54 +0530

    Add transfer requests to be approve table

--- src/app/components/transfers/transfers.component.ts
+++ src/app/components/transfers/transfers.component.ts
@@ -3,6 +3,8 @@ import {OverallInfoService} from 'app/services/overallInfo/overall-info.service'
 import {MatPaginator, MatSort, MatTableDataSource} from '@angular/material';
 import {Transfer} from 'app/models/Transfer';
 import {Observable} from 'rxjs';
+import {DocumentReference} from '@angular/fire/firestore';
+import {SwalService} from 'app/services/swal/swal.service';
 
 @Component({
   selector: 'app-transfers',
@@ -10,23 +12,44 @@ import {Observable} from 'rxjs';
   styleUrls: ['./transfers.component.scss']
 })
 export class TransfersComponent implements OnInit {
-  displayedColumns: string[] = ['issue_id', 'date_due', 'renewals', 'title', 'issuedate', 'status'];
-  dataSource: MatTableDataSource<Transfer>;
+  allTransferColumns: string[] = [
+    'issue_id',
+    'date_due',
+    'renewals',
+    'title',
+    'issuedate',
+    'status'
+  ];
+  toTransferColumns: string[] = [
+    'issue_id',
+    'title',
+    'date_due',
+    'issuedate',
+    'sender',
+    'receiver',
+    'status'
+  ];
+  allTransfers: MatTableDataSource<Transfer>;
+  toTransfers: MatTableDataSource<Transfer>;
   isLoadingResults = true;
   transferRequests: Observable<Transfer[]>;
-  @ViewChild(MatPaginator) paginator: MatPaginator;
-  @ViewChild(MatSort) sort: MatSort;
-  constructor(private info: OverallInfoService) {}
+  @ViewChild(MatPaginator) allTransferPaginator: MatPaginator;
+  @ViewChild(MatPaginator) toTransferPaginator: MatPaginator;
+  @ViewChild(MatSort) allTransferSort: MatSort;
+  constructor(private info: OverallInfoService, private swal: SwalService) {}
 
   ngOnInit() {
     this.transferRequests = this.info.getTransferRequests();
     this.transferRequests.subscribe(data => {
       const requests = Object.keys(data).map(key => data[key]);
-      console.log('requests', requests);
-      this.dataSource = new MatTableDataSource(requests);
-      this.dataSource.paginator = this.paginator;
-      this.dataSource.sort = this.sort;
+      this.allTransfers = new MatTableDataSource(requests);
+      this.allTransfers.paginator = this.allTransferPaginator;
+      this.allTransfers.sort = this.allTransferSort;
       this.isLoadingResults = false;
+
+      this.toTransfers = new MatTableDataSource(
+        requests.filter((element: Transfer) => element.status === 'toBeApproved')
+      );
     });
   }
 
@@ -35,9 +58,35 @@ export class TransfersComponent implements OnInit {
    * @param filterValue
    */
   applyFilter(filterValue: string) {
-    this.dataSource.filter = filterValue.trim().toLowerCase();
-    if (this.dataSource.paginator) {
-      this.dataSource.paginator.firstPage();
+    this.allTransfers.filter = filterValue.trim().toLowerCase();
+    if (this.allTransfers.paginator) {
+      this.allTransfers.paginator.firstPage();
     }
   }
+
+  /**
+   * Approve a book transfer request
+   * This will update the book request transfer state to 'approved'
+   */
+  approveTransfer = (docRef: DocumentReference) => {
+    docRef
+      .update({status: 'Approved'})
+      .then(() => this.swal.viewSuccessMessage('Success', 'Transfer Approved'));
+  };
+
+  /**
+   * Reject a book transfer request
+   * This will update the book request transfer state to 'rejected'
+   */
+  rejectTransfer = (docRef: DocumentReference) => {
+    console.log('rejecting');
+    this.swal.confirmMessageWithText(
+      'Confirm',
+      'Do you want to reject the request if so please add a descripiton',
+      message => {
+        console.log('value', message);
+        docRef.update({status: 'Rejected', message});
+      }
+    );
+  };
 }
