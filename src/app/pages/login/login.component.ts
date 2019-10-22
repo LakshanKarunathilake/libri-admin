f24a1d3891eda1c400da6fa5dc5d817d7a1a0c51
lakshan <manualsplash@gmail.com>
2019-10-22 20:42:17 +0530

    Add login and register ability

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,6 +1,8 @@
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
+import {SwalService} from 'app/services/swal/swal.service';
+import {AdminService} from 'app/services/admin/admin.service';
 
 @Component({
   selector: 'app-login',
@@ -10,17 +12,46 @@ import {Router} from '@angular/router';
 export class LoginComponent implements OnInit {
   loginForm: FormGroup;
 
-  constructor(private fb: FormBuilder, private router: Router) {}
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private swal: SwalService,
+    private admin: AdminService
+  ) {}
 
   ngOnInit() {
     this.loginForm = this.fb.group({
       userName: ['', [Validators.required]],
-      userPassword: ['', Validators.required]
+      userPassword: ['', Validators.required],
+      adminPassword: ['', Validators.required]
     });
   }
 
   login = () => {
-    console.log('Logging in to the system');
-    this.router.navigateByUrl('dashboard');
+    const username = this.loginForm.controls['userName'].value;
+    const password = this.loginForm.controls['userPassword'].value;
+    this.admin
+      .authenticate(username, password)
+      .then(reply => {
+        console.log('reply', reply);
+        this.router.navigateByUrl('dashboard');
+      })
+      .catch(error => {
+        console.log('error', error);
+        this.swal.viewErrorMessage(
+          'Invalid',
+          'Sorry you authorization details are invalid, please check agai'
+        );
+      });
+  };
+
+  /**
+   * Register admin
+   */
+  register = () => {
+    const username = this.loginForm.controls['userName'].value;
+    const password = this.loginForm.controls['userPassword'].value;
+    const adminPassword = this.loginForm.controls['adminPassword'].value;
+    this.admin.registerAdmin(username, password, adminPassword);
   };
 }
